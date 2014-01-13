289e17ddbedc15a0cbd45a5491015b4259a4dfd4
Mehmet Akif Tütüncü <m.akif.tutuncu@gmail.com>
2014-01-13 15:19:32 +0200

    Update config.php

--- database/config.php
+++ database/config.php
@@ -4,7 +4,7 @@
   
 define("HOST", "localhost");			// The host you want to connect to.
 define("USER", "twittercik");			// The database username. 
-define("PASSWORD", "zMaaBFJNNWQzCWFf");	// The database password. 
+define("PASSWORD", "");	// The database password. 
 define("DATABASE", "twittercik");		// The database name.
 
 ?>
